b31643738a5e104d2a8769efda7370c8997b4386
Trangar <victor.koenders@gmail.com>
2016-10-21 10:09:12 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
+@echo off
+echo -- Restoring references --
 "D:\Tools\nuget.exe" restore
 if %errorlevel% neq 0 ( exit %errorlevel% )
+echo -- Building --
 "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe" DataCenter_IRCConnector.sln /t:rebuild /p:Configuration=Release;Platform="Any CPU";DebugSymbols=false;DebugType=None /m
